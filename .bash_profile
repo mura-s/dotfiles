8e5c1630f1e8e598afc33be29b93ec17a064c7a8
mura-s <seiya.muramatsu1@gmail.com>
2014-10-26 01:06:55 +0900

    bashとpercolの設定を削除

--- .bash_profile
+++
@@ -1,9 +0,0 @@
-# bashrc
-if [ -f ~/.bashrc ] ; then
-	source ~/.bashrc
-fi
-
-# PATH
-# rbenv
-export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init -)"
--- ln_dotfiles.sh
+++ ln_dotfiles.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-ln -s $GOPATH/src/github.com/mura-s/dotfiles/.bash_profile ~/.bash_profile
-ln -s $GOPATH/src/github.com/mura-s/dotfiles/.bashrc ~/.bashrc
 ln -s $GOPATH/src/github.com/mura-s/dotfiles/.gitconfig ~/.gitconfig
 ln -s $GOPATH/src/github.com/mura-s/dotfiles/.m2 ~/.m2
-ln -s $GOPATH/src/github.com/mura-s/dotfiles/.percol.d ~/.percol.d
 ln -s $GOPATH/src/github.com/mura-s/dotfiles/.peco ~/.peco
 ln -s $GOPATH/src/github.com/mura-s/dotfiles/.tigrc ~/.tigrc
 ln -s $GOPATH/src/github.com/mura-s/dotfiles/.tmux.conf ~/.tmux.conf
